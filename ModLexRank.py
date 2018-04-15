82cf763fd21ba26b3e1037cefa043f9b0d76b9ee
Nels Salminen <n.salminen@me.com>
2018-04-15 13:31:33 +0200

    Added preprocessing and improved algorithm

--- ModLexRank.py
+++ ModLexRank.py
@@ -1,83 +1,117 @@
 from lexrank import STOPWORDS, LexRank
-import math
-from collections import Counter, defaultdict
-import nltk
-import re
-from urlextract import URLExtract
-import numpy as np
-
 from lexrank.algorithms.power_method import stationary_distribution
 from lexrank.utils.text import tokenize
 
+import numpy as np
+from tqdm import tqdm
+import math
+
+
 class ModLexRank(LexRank):
-	"""docstring for ModLexRank"""
-	def validateSentence(self, s):
-		alphaCount = sum(c.isalpha() for c in s)
-		if (alphaCount < 15 or alphaCount > 130):
-			return False
-		extractor = URLExtract()
-		urls = extractor.find_urls(s)
-		if (urls):
-			return False
-		return True
+    """docstring for ModLexRank"""
+    def __init__(
+        self,
+        documents,
+        stopwords=None,
+        keep_numbers=False,
+        keep_emails=False,
+        include_new_words=True,
+    ):
+        if stopwords is None:
+            self.stopwords = set()
+        else:
+            self.stopwords = stopwords
 
-	def __init__(
-		self,
-		documents,
-		stopwords=None,
-		keep_numbers=False,
-		keep_emails=False,
-		include_new_words=True,
-	):
-		if stopwords is None:
-			self.stopwords = set()
-		else:
-			self.stopwords = stopwords
+        self.doc_sentences = list()
+        self.keep_numbers = keep_numbers
+        self.keep_emails = keep_emails
+        self.keep_urls = False
+        self.include_new_words = include_new_words
 
-		self.doc_sentences = list()
-		self.keep_numbers = keep_numbers
-		self.keep_emails = keep_emails
-		self.keep_urls = False
-		self.include_new_words = include_new_words
+        bags_of_words = []
 
-		bags_of_words = []
+        for doc in documents:
+            doc_words = set()
 
-		for doc in documents:
-			doc_words = set()
+            for sentence in doc:
+                self.doc_sentences.append(sentence)
+                words = self.tokenize_sentence(sentence)
+                doc_words.update(words)
 
-			for sentence in doc:
-				articleSentences = nltk.sent_tokenize(sentence)
-				articleSentences[:] = [tup for tup in articleSentences if self.validateSentence(s=tup)]
-				self.doc_sentences = self.doc_sentences + articleSentences
-				words = self.tokenize_sentence(sentence)
-				doc_words.update(words)
+            if doc_words:
+                bags_of_words.append(doc_words)
 
-			if doc_words:
-				bags_of_words.append(doc_words)
+        if not bags_of_words:
+            raise ValueError('documents are not informative')
+        self.bags_of_words = bags_of_words
+        self.idf_score = self._calculate_idf()
 
-		if not bags_of_words:
-			raise ValueError('documents are not informative')
-		self.bags_of_words = bags_of_words
-		self.idf_score = self._calculate_idf()
+    def _markov_matrix_discrete(self, similarity_matrix, threshold):
+        markov_matrix = np.zeros(similarity_matrix.shape)
+        for i in range(len(similarity_matrix)):
+            columns = np.where(similarity_matrix[i] > threshold)[0]
+            markov_matrix[i, columns] = 1 / len(columns)
 
-	def get_summary(
-		self,
-		summary_size=1,
-		threshold=.03,
-		discretize=True,
-		fast_power_method=True,
-	):
-		if not isinstance(summary_size, int) or summary_size < 1:
-			raise ValueError('\'summary_size\' should be a positive integer')
+        return markov_matrix
 
-		lexrank = self.rank_sentences(
-			self.doc_sentences,
-			threshold=threshold,
-			discretize=discretize,
-			fast_power_method=fast_power_method,
-		)
+    def rank_sentences(
+        self,
+        sentences,
+        threshold=.03,
+        discretize=True,
+        fast_power_method=True,
+        normalize=False,
+    ):
+        if not isinstance(threshold, float) or not 0 <= threshold < 1:
+            raise ValueError(
+                '\'threshold\' should be a floating-point number '
+                'from the interval [0, 1)',
+            )
 
-		sorted_ix = np.argsort(lexrank)[::-1]
-		summary = [self.doc_sentences[i] for i in sorted_ix[:summary_size]]
+        tf_scores = [
+            self._calculate_tf(self.tokenize_sentence(sentence))
+            for sentence in sentences
+        ]
+        similarity_matrix = self._calculate_similarity_matrix(tf_scores)
+        np.savetxt('text.txt', similarity_matrix, delimiter=' ', fmt='%.2f')
+        if discretize:
+            markov_matrix = self._markov_matrix_discrete(
+                similarity_matrix,
+                threshold=threshold,
+            )
 
-		return summary
+        else:
+            markov_matrix = self._markov_matrix(similarity_matrix)
+
+        lexrank = stationary_distribution(
+            markov_matrix,
+            increase_power=fast_power_method,
+        )
+
+        if normalize:
+            max_val = max(lexrank)
+            lexrank = [val / max_val for val in lexrank]
+
+        return lexrank
+
+    def get_summary(
+        self,
+        summary_size=1,
+        threshold=.03,
+        discretize=True,
+        fast_power_method=True,
+    ):
+        if not isinstance(summary_size, int) or summary_size < 1:
+            raise ValueError('\'summary_size\' should be a positive integer')
+
+        lexrank = self.rank_sentences(
+            sentences=self.doc_sentences,
+            threshold=threshold,
+            discretize=discretize,
+            fast_power_method=fast_power_method,
+        )
+        sorted_ix = np.argsort(lexrank)[::-1]
+        relative_summary_size = math.ceil(len(sorted_ix) * 0.05)
+        summary = [self.doc_sentences[i] for i in sorted_ix[:relative_summary_size]]
+
+        return summary
--- main.py
+++ main.py
@@ -1,36 +1,79 @@
-import nltk
-import time
-import sys
-from urllib.request import Request, urlopen
-from ModLexRank import ModLexRank
-from lexrank import STOPWORDS
-from newspaper import Article, Config
 from SourceArticle import SourceArticle
-from tqdm import tqdm, trange
+from tqdm import tqdm
+from lexrank import STOPWORDS, LexRank
+from path import Path
+from ModLexRank import ModLexRank
+
 
 def readDataFile(path, sourceArticleList):
-	with open(path) as f:
-		content = [x.strip('\n') for x in f] 
+    with open(path) as f:
+        content = [x.strip('\n') for x in f]
 
-	idx = 0
-	pbar = tqdm(total=len(content), unit=" lines", desc="Reading " + path)
-	while idx < len(content):
-		if (content[idx] == 'new node'):
-			idx = idx + 1
-			sourceArticleList.append(SourceArticle(content[idx]))
-			pbar.update(1)
-		elif (content[idx] != 'list'):
-			sourceArticleList[-1].rebutalUrlList.append(content[idx])
-		pbar.update(1)
-		idx = idx + 1
+    idx = 0
+    pbar = tqdm(total=len(content), unit=" lines", desc="Reading " + path)
+    while idx < len(content):
+        if (content[idx] == 'new node'):
+            idx = idx + 1
+            sourceArticleList.append(SourceArticle(content[idx]))
+            pbar.update(1)
+        elif (content[idx] != 'list'):
+            sourceArticleList[-1].rebuttalUrlList.append(content[idx])
+        pbar.update(1)
+        idx = idx + 1
+
 
 sourceArticleList = list()
 readDataFile("output.txt", sourceArticleList)
-pbar = tqdm(total=len(sourceArticleList), unit="article", desc=("Summarizing " + str(len(sourceArticleList)) + " articles"))
+pbar = tqdm(total=len(sourceArticleList), unit="article",
+            desc=("Summarizing " + str(len(sourceArticleList)) + " articles"))
+threshold = 0.5
 for idx, val in enumerate(sourceArticleList):
-	summary = sourceArticleList[idx].summarizeRebutals(size=10, summary_threshold = 0.2)
-	with open('summary' + str(idx) + '.txt', mode='wt', encoding='utf-8') as summaryFile:
-		summaryFile.write(' '.join(summary))
-	pbar.update(1)
-	print(summary)
-pbar.close()
+    sourceArticleList[idx].downloadRebuttals()
+    summary = sourceArticleList[idx].summarizeRebuttals(size=10, summary_threshold=threshold)
+    with open('summary' + str(idx) + '_t' + str(threshold) + '.txt', mode='wt', encoding='utf-8') \
+            as summaryFile:
+                summaryFile.write(' '.join(summary))
+    pbar.update(1)
+    pbar.close()
+
+# documents = []
+# documents_dir = Path('bbc/politics')
+
+# for file_path in documents_dir.files('*.txt'):
+#     with file_path.open(mode='rt', encoding='utf-8') as fp:
+#         documents.append(fp.readlines())
+# print(documents)
+# lxr = ModLexRank(documents, stopwords=STOPWORDS['en'])
+
+# sentences = [
+#     'One of David Cameron\'s closest friends and Conservative allies, '
+#     'George Osborne rose rapidly after becoming MP for Tatton in 2001.',
+
+#     'Michael Howard promoted him from shadow chief secretary to the '
+#     'Treasury to shadow chancellor in May 2005, at the age of 34.',
+
+#     'Mr Osborne took a key role in the election campaign and has been at '
+#     'the forefront of the debate on how to deal with the recession and '
+#     'the UK\'s spending deficit.',
+
+#     'Even before Mr Cameron became leader the two were being likened to '
+#     'Labour\'s Blair/Brown duo. The two have emulated them by becoming '
+#     'prime minister and chancellor, but will want to avoid the spats.',
+
+#     'Before entering Parliament, he was a special adviser in the '
+#     'agriculture department when the Tories were in government and later '
+#     'served as political secretary to William Hague.',
+
+#     'The BBC understands that as chancellor, Mr Osborne, along with the '
+#     'Treasury will retain responsibility for overseeing banks and '
+#     'financial regulation.',
+
+#     'Mr Osborne said the coalition government was planning to change the '
+#     'tax system \"to make it fairer for people on low and middle '
+#     'incomes\", and undertake \"long-term structural reform\" of the '
+#     'banking sector, education and the welfare state.',
+# ]
+
+# # get summary with classical LexRank algorithm
+# summary = lxr.get_summary(sentences, summary_size=2, threshold=.1)
+# print(summary)
